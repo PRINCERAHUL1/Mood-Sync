ab4802caf049357a302b102e68aec60a98d5f823
PRINCERAHUL1 <rmrahul258@gmail.com>
2025-03-03 10:18:05 +0530

    Fixed Routing issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // ✅ Use HashRouter
 import App from "./App";
 import Playlists from "./pages/Playlists";
 import MoodInput from "./pages/MoodInput";
@@ -9,7 +9,7 @@ import Logout from "./pages/Logout";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
+    <Router>  {/* ✅ Changed to HashRouter */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/playlists" element={<Playlists />} />
